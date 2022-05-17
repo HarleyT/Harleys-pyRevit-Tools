a20e687903947b2bf737904213a6b0e1b8c393c7
HarleyT <harley.trappitt@gmail.com>
2022-05-17 10:53:31 +1000

    generic models only

--- pyRevitTest.tab/pyRevitTest.panel/MyFirstCommand.pushbutton/script.py
+++ pyRevitTest.tab/pyRevitTest.panel/MyFirstCommand.pushbutton/script.py
@@ -12,7 +12,7 @@ family_dict = {}
 
 for family in revit.query.get_families(revit.doc, only_editable=True):
 
-    if family.FamilyCategory:
+    if family.FamilyCategory == "Generic Models":
 
         family_dict[
 
