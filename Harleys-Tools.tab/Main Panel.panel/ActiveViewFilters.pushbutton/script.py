6aa050a33bb8fdd3e0058f543402bd3ebb3cd2bb
HarleyT <harley.trappitt@gmail.com>
2022-07-07 15:19:16 +1000

    updates

--- Harleys-Tools.tab/Main Panel.panel/ActiveViewFilters.pushbutton/script.py
+++ Harleys-Tools.tab/Main Panel.panel/ActiveViewFilters.pushbutton/script.py
@@ -137,7 +137,7 @@ view_filters = {}
 element = []
 elements = []
 
-for e in revit.query.get_view_filters(current_view):
+for e in revit.query.GetFilters(current_view):
     if element:
         view_filters[
             "%s: %s" % (element.Name)
