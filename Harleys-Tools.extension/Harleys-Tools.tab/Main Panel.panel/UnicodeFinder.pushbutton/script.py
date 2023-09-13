247c9265a8f73dc5a7a2a65523b54a071cdc7a75
HarleyT <harley.trappitt@gmail.com>
2023-09-13 11:24:23 +1000

    unicode finder updates

--- Harleys-Tools.extension/Harleys-Tools.tab/Main Panel.panel/UnicodeFinder.pushbutton/script.py
+++ Harleys-Tools.extension/Harleys-Tools.tab/Main Panel.panel/UnicodeFinder.pushbutton/script.py
@@ -1,13 +1,13 @@
-import clr
-clr.AddReference('RevitAPI')
-clr.AddReference('System.Windows.Forms')
-clr.AddReference('IronPython.Wpf')
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter
-from pyrevit import revit, script, UI
-import re
-import wpf
-xamlfile = script.get_bundle_file('ui.xaml')
-from System import Windows
+#import clr
+#clr.AddReference('RevitAPI')
+#clr.AddReference('System.Windows.Forms')
+#clr.AddReference('IronPython.Wpf')
+#from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, BuiltInParameter
+#from pyrevit import revit, script, UI
+#import re
+#import wpf
+#xamlfile = script.get_bundle_file('ui.xaml')
+#from System import Windows
 
 
 # Import the XAML layout
@@ -15,39 +15,90 @@ from System import Windows
 #from System.Windows import Application, Window
 
 # Define a regular expression pattern to match Unicode characters
-unicode_pattern = re.compile('[^\x00-\x7F]+')
+#unicode_pattern = re.compile('[^\x00-\x7F]+')
 
 # Function to check if a string contains Unicode characters
-def contains_unicode(text):
-    return bool(unicode_pattern.search(text))
+#def contains_unicode(text):
+#    return bool(unicode_pattern.search(text))
 
 # Function to search for Unicode characters in sheet numbers
-def search_sheets_for_unicode(sender, e):
-    # Get all sheets in the current Revit project
-    sheets_collector = FilteredElementCollector(revit.doc).OfCategory(BuiltInCategory.OST_Sheets)
-    sheets = list(sheets_collector)
+#def search_sheets_for_unicode(sender, e):
+#    # Get all sheets in the current Revit project
+#    sheets_collector = FilteredElementCollector(revit.doc).OfCategory(BuiltInCategory.OST_Sheets)
+#    sheets = list(sheets_collector)
 
     # Initialize a list to store sheets with Unicode characters
-    sheets_with_unicode = []
+#    sheets_with_unicode = []
 
     # Iterate through each sheet and check its sheet number
-    for sheet in sheets:
-        sheet_number = sheet.get_Parameter(BuiltInParameter.SHEET_NUMBER).AsString()
-        if contains_unicode(sheet_number):
-            sheets_with_unicode.append(sheet_number)
+#    for sheet in sheets:
+#        sheet_number = sheet.get_Parameter(BuiltInParameter.SHEET_NUMBER).AsString()
+#        if contains_unicode(sheet_number):
+#            sheets_with_unicode.append(sheet_number)
 
     # Display the sheets with Unicode characters in the resultTextBox
-    if sheets_with_unicode:
-        resultTextBox.Text = "\n".join(sheets_with_unicode)
-    else:
-        resultTextBox.Text = "No sheets with Unicode characters found."
+#    if sheets_with_unicode:
+#        resultTextBox.Text = "\n".join(sheets_with_unicode)
+#    else:
+#        resultTextBox.Text = "No sheets with Unicode characters found."
 
 # Create the main window
-class MainWindow(Windows.Window):
+#class MainWindow(Windows.Window):
+#    def __init__(self):
+#        wpf.LoadComponent(self, xamlfile)
+#        self.SearchButton_Click += self.search_sheets_for_unicode
+
+#if __name__ == '__main__':
+#    #Application().Run(MainWindow())
+#    MainWindow().ShowDialog()
+
+
+
+# dependencies
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('IronPython.Wpf')
+
+# find the path of ui.xaml
+from pyrevit import UI
+from pyrevit import script
+xamlfile = script.get_bundle_file('ui.xaml')
+
+# import WPF creator and base Window
+import wpf
+from System import Windows
+
+class MyWindow(Windows.Window):
     def __init__(self):
         wpf.LoadComponent(self, xamlfile)
-        self.SearchButton_Click += self.search_sheets_for_unicode
 
-if __name__ == '__main__':
-    #Application().Run(MainWindow())
-    MainWindow().ShowDialog()
+    # Define a regular expression pattern to match Unicode characters
+    unicode_pattern = re.compile('[^\x00-\x7F]+')
+
+    # Function to check if a string contains Unicode characters
+    def contains_unicode(text):
+        return bool(unicode_pattern.search(text))
+
+    # Function to search for Unicode characters in sheet numbers
+    def search_sheets_for_unicode(sender, e):
+        # Get all sheets in the current Revit project
+        sheets_collector = FilteredElementCollector(revit.doc).OfCategory(BuiltInCategory.OST_Sheets)
+        sheets = list(sheets_collector)
+
+        # Initialize a list to store sheets with Unicode characters
+        sheets_with_unicode = []
+
+        # Iterate through each sheet and check its sheet number
+        for sheet in sheets:
+            sheet_number = sheet.get_Parameter(BuiltInParameter.SHEET_NUMBER).AsString()
+            if contains_unicode(sheet_number):
+                sheets_with_unicode.append(sheet_number)
+
+        # Display the sheets with Unicode characters in the resultTextBox
+        if sheets_with_unicode:
+            resultTextBox.Text = "\n".join(sheets_with_unicode)
+        else:
+            resultTextBox.Text = "No sheets with Unicode characters found."
+
+# Let's show the window (modal)
+MyWindow().ShowDialog()
