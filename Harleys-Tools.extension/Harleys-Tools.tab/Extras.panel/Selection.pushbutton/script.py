78be56917511e37cfeb7eec2e030a66e5d2504d7
HarleyT <harley.trappitt@gmail.com>
2023-10-24 15:57:27 +1100

    commit

--- Harleys-Tools.extension/Harleys-Tools.tab/Extras.panel/Selection.pushbutton/script.py
+++ Harleys-Tools.extension/Harleys-Tools.tab/Extras.panel/Selection.pushbutton/script.py
@@ -1,9 +1,10 @@
 from example import test_print
 from Snippets._selection import get_selected_elements
+from Autodesk.Revit.DB import *
 
 # Variables
-uidoc = __revit__.ActiveUIDocument
-doc   = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument              #type: UIDocument
+doc   = __revit__.ActiveUIDocument.Document     #type: Document
 
 
 # Test Print
@@ -12,3 +13,4 @@ test_print()
 # Get Selected Elements
 selected_elements = get_selected_elements(uidoc)
 print(selected_elements)
+
